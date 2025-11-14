595f2c56ce1167fdabf3ab068725389ae858453c
Souhill <71548291+uvsq22007899@users.noreply.github.com>
2025-11-14 12:13:59 +0100

    README + début de code
    README prêt + début de code

---
+++ README.md
@@ -0,0 +1,26 @@
+# Arabic Typo Wallpaper
+
+Projet Web Tool en p5 js pour le cours de creative coding .  
+L'idée c'est de faire un mini web tool qui permet de générer un fond d'écran avec un prenom écrit en **arabe**.
+
+## Le concept :
+L'utilisateur met son prénom en en francais (typo Latine) et le webtool le traduit en arabe  
+(ex souhil ->  سهيل)  
+et le code crée un fond d'écran avec un **gros texte** + un **slash coloré** derriere.
+
+L'utilisateur pourra donc changer :
+- le texte arabe
+- la couleur principal (accent)
+
+## L'avancé du projet':
+Dans cette premiere version du webtool, l'utilisateur peut écrire son prenom et le webtool le traduit en typo arabe et l'affiche en grand.
+
+
+## Les langages utilisés :
+- P5.JS
+- HTML 
+- CSS
+- JS
+
+
+
--- sketch.js
+++ sketch.js
@@ -1,7 +1,79 @@
+let inputLatin;
+let arabicText = "";
+let infoText = "Écris ton prénom :";
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(windowWidth, windowHeight);
+  textAlign(CENTER, CENTER);
+  textFont("sans-serif");
+
+  inputLatin = createInput("");
+  inputLatin.position(20, 20);       
+  inputLatin.size(220);
+  inputLatin.input(updateArabic);    
 }
 
 function draw() {
-  background(220);
+  background(20);
+
+
+  fill(200);
+  textSize(18);
+  text(infoText, width / 2, 50);
+
+
+  fill(255, 200, 200);
+  textSize(80);
+  text(arabicText, width / 2, height / 2);
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
+
+function updateArabic() {
+  const latin = inputLatin.value().toLowerCase();
+  arabicText = latinToArabic(latin);
+}
+
+function latinToArabic(str) {
+  const map = {
+    "a": "ا",
+    "b": "ب",
+    "c": "ك",
+    "d": "د",
+    "e": "e",   
+    "f": "ف",
+    "g": "ج",
+    "h": "ه",
+    "i": "ي",
+    "j": "ج",
+    "k": "ك",
+    "l": "ل",
+    "m": "م",
+    "n": "ن",
+    "o": "و",
+    "p": "ب",
+    "q": "ق",
+    "r": "ر",
+    "s": "س",
+    "t": "ت",
+    "u": "و",
+    "v": "ف",
+    "w": "و",
+    "x": "كس",
+    "y": "ي",
+    "z": "ز",
+    " ": " "
+  };
+
+  let result = "";
+  for (let char of str) {
+    if (map[char]) {
+      result += map[char];
+    } else {
+      result += char; 
+    }
+  }
+  return result;
 }
